73dc5e83c92ef73ec9271d04aa73af72aaaff725
gklf-pixel <57415409+gklf-pixel@users.noreply.github.com>
2022-11-10 00:04:15 +0300

    Add files via upload

---
+++ laba2.2/laba2.2/laba2.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, c, d, e, i;
+	for (i = 1000; i < 10000; i++)
+	{
+		a = i;
+		b = a % 10;
+		a = a / 10;
+		c = a % 10;
+		a = a / 10;
+		d = a % 10;
+		a = a / 10;
+		if (a + b + c + d == 30 && i % 2 == 0 && i % 7 == 0 && i % 11 == 0)
+			e = i;
+	}
+	printf("%d", e);
+	return 0;
+}
